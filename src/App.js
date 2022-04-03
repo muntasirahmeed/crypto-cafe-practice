77604827ad141ce45282b6b3b404b843bec8fa03
muntasir ahmeed <muntasirahmed5255@gmail.com>
2022-04-03 14:58:52 +0600

    not found page add and loading add

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import UsAddress from './components/Contact/UsAdress';
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
 import Home from './components/Home/Home';
+import NotFound from './components/NotFound/NotFound';
 
 function App() {
   return (
@@ -25,6 +26,7 @@ function App() {
         </Route>
         <Route path='/coindetails/:id' element={<CoinDetails></CoinDetails>}></Route>
         <Route path='/about' element={<About></About>}> </Route>
+        <Route path='*' element={<NotFound></NotFound>}></Route>
       </Routes>
       <Footer></Footer>
     </div>
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import CustomLink from '../CustomLink/CustomLink';
 
 const Header = () => {
     return (
         <nav className='flex items-center justify-center md:justify-between bg-white px-12 py-8 sticky top-0 max-w-screen-2xl mx-auto'>
             <div className='hidden md:block'>
-                <h1 className='text-xl font-bold text-gray-600'>Crypto Cafe</h1>
+                <Link to='/home'><h1 className='text-xl font-bold text-gray-600'>Crypto Cafe</h1></Link>
             </div>
             <div className='flex gap-4 text-xl'>
                 <CustomLink to='/home'>Home</CustomLink> 
